41005f7fd71dfd76b22a40d4d001569b71b8a97c
Mike Maquera <mike.maquera212@gmail.com>
2019-11-30 12:59:35 -0500

    added tabs to front page

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ async function search() {
     var i = 0;
     var result = data.priceData[0].closeAdj
     var result2 = data.priceData[4].closeAdj
+
+    //loop through the data of the api and push it to the graph function 
    // for(i;i<data.length;i++){
       //   
         
@@ -31,8 +33,6 @@ async function search() {
 
     
 }
-
-
  function graph(data1,data2) {
 
     var a = data1;
@@ -46,63 +46,21 @@ async function search() {
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
+                label: 'COMPANY NAME HERE',
                 data: [a, b],
 
 
- }
+                     }]
+
+            }
+
+    })
+
+}
 
 
+     
 
-
-/*
-    window.addEventListener('load', function () {
-        
-    
-    var canvas = document.getElementById('myChart')
-    var ctx = canvas.getContext("2d");
-    ctx.fillRect(0, 0, 150, 75);
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-
-});
-
-*/
 //consenus data
 
 
@@ -118,8 +76,15 @@ var myChart = new Chart(ctx, {
 
 //alt data
 
-//buy or sell signals from major companys
+//stocktwits api data 
 
 
-
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    document.getElementById(cityName).style.display = "block"; 
+  }
 
